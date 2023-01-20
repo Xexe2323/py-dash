3697445a05fa99ddd8aca766593dd0098d7c4a34
Xexe2323 <111385114+Xexe2323@users.noreply.github.com>
2023-01-21 00:51:14 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%y#3qaz7#a&ysm&sxl=xn27y7i5)=x1d6eeb6+o2_=&l=du9@7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
